a761a392a21ca5355182670f645f0061510b671d
Kamila Kulczyk <kamila.kulczyk@gmail.com>
2022-12-05 10:40:30 +0100

    first commit

---
+++ kalendarz/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            header test test test test test jhgjhg
+        </div>
+    );
+};
+
+export default Header;
