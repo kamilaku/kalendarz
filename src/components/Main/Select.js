298e04a925dd6955cd2cfc3b0c9cb751bb9ad330
Kamila <107698911+kamilaku@users.noreply.github.com>
2022-12-08 12:06:05 +0100

    select

--- src/components/Main/Select.js
+++ src/components/Main/Select.js
@@ -18,7 +18,7 @@ const Select = ({def, data, handleSelect}) => {
         <div className='container'>
             <select 
                 value={selected}
-                className='select-year' 
+                className='select' 
                 onChange={choose}
             >{options}
             </select>
@@ -27,31 +27,5 @@ const Select = ({def, data, handleSelect}) => {
 }
 
 
-// const SelectMonth = ({def, data, handleSelect}) => {
-//     const [selected, setSelected] = useState(def);
-
-//     const choose = (event) => {
-//         setSelected(event.target.value);
-//         if (handleSelect) {
-//             handleSelect(event.target.value);
-//         }
-//     };
-
-//     const options = data.map((option) => 
-//         <option key={option.value+option.title} value={option.value}>{option.title}</option>
-//     );
-
-//     return ( 
-//         <div className='container'>
-//             <select 
-//                 value={selected}
-//                 className='select-month' 
-//                 onChange={choose}
-//             >{options}
-//             </select>
-//         </div>
-//     )
-// }
 
 export default Select;
-// export { SelectMonth };
